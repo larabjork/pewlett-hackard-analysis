ab3e9f546ecc0c4a7e02fcbb9b274c97d134405a
larabjork <lara.m.bjork@gmail.com>
2022-10-17 15:07:46 -0700

    update schema.sql for two tables

--- schema.sql
+++ schema.sql
@@ -41,7 +41,6 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_emp (
@@ -51,5 +50,4 @@ CREATE TABLE dept_emp (
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
 );
